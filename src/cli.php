5ed3620b2239bde55bd2a58c824b7eb1b38f9d56
Petya <donald@trump.com>
2019-09-05 07:56:20 -0700

    small edits

--- src/cli.php
+++ src/cli.php
@@ -4,6 +4,9 @@ namespace BrainGames\Cli;
 
 use function \cli\line;
 
-line('Welcome to the Brain Game!');
-$name = \cli\prompt('May I have your name?');
-line("Hello, %s!", $name);
+function run()
+{
+    line('Welcome to the Brain Game!');
+    $name = \cli\prompt('May I have your name?');
+    line("Hello, %s!", $name);
+}
