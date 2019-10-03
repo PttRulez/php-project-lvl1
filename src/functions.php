74dc7380655724d50e93a06cd87affcfe1033155
Petya <donald@trump.com>
2019-10-03 05:38:10 -0700

    Calc game

--- src/functions.php
+++ src/functions.php
@@ -28,7 +28,7 @@ function sayHello($name)
 function runEvenGame($name)
 {
     for ($i = 0; $i < 3; $i++) {
-        $number = rand();
+        $number = rand(1, 100);
         line("Question: %s", $number);
         $answer = \cli\prompt('Your answer');
         $odd =  ($number % 2 == 0) ? true : false;
@@ -37,6 +37,44 @@ function runEvenGame($name)
             line("Correct!");
         } else {
             $correctAnswer = ($odd == true) ? 'yes' : 'no';
+            line("'%s' is wrong answer ;(. Correct answer was '%s'", $answer, $correctAnswer);
+            line("Let's try again, %s", $name);
+            return;
+        }
+    }
+    line("Congratulations, %s!", $name);
+}
+
+function runCalcGame($name)
+{
+    for ($i = 0; $i < 3; $i++) {
+        $number1 = rand(1, 100);
+        $number2 = rand(1, 100);
+        $operand = rand(1,3);
+        $string = "";
+        $correctAnswer = 0;
+
+        switch ($operand) {
+            case 1:
+                $string = $number1 . ' + ' . $number2;
+                $correctAnswer = $number1 + $number2;
+                break;
+            case 2:
+                $string = $number1 . ' - ' . $number2;
+                $correctAnswer = $number1 - $number2;
+                break;
+            case 3:
+                $string = $number1 . ' * ' . $number2;
+                $correctAnswer = $number1 * $number2;
+                break;
+        }
+
+        line("Question: %s", $string);
+        $answer = \cli\prompt('Your answer');
+
+        if ($answer == $correctAnswer) {
+            line("Correct!");
+        } else {
             line("'%s' is wrong answer ;(. Correct answer was '%s'", $answer, $correctAnswer);
             line("Let's try again, %s", $name);
             return;
