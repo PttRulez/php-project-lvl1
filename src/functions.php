ee3c353542853b4a3fbdcb8b0d4b0ab46238f630
Petya <donald@trump.com>
2019-10-05 23:58:19 -0700

    Brain-progression created

--- src/functions.php
+++ src/functions.php
@@ -117,3 +117,35 @@ function runGcdGame($name)
     }
     line("Congratulations, %s!", $name);
 }
+
+function runProgressionGame($name)
+{
+    for ($i = 0; $i < 3; $i++) {
+        $row = [];
+        $row[0] = rand(1, 10);
+        $step = rand(1, 6);
+        $hiddenIndex = rand(0, 9);
+        $string = '';
+
+        for ($j = 1; $j < 10; $j++) {
+            $row[$j] = $row[$j - 1] + $step;
+            if ($j == $hiddenIndex) {
+                $string .= '.. ';
+            } else {
+                $string .= $row[$j] . ' ';
+            }
+        }
+
+        line("Question: %s", $string);
+        $answer = \cli\prompt('Your answer');
+
+        if ($answer == $row[$hiddenIndex]) {
+            line("Correct!");
+        } else {
+            line("'%s' is wrong answer ;(. Correct answer was '%s'", $answer, $row[$hiddenIndex]);
+            line("Let's try again, %s", $name);
+            return;
+        }
+    }
+    line("Congratulations, %s!", $name);
+}
