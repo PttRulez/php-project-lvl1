d0ba5f6aff557cde8e50bad639417777f0ca2626
Petya <donald@trump.com>
2019-10-07 00:40:16 -0700

    Prepared project

---
+++ src/games/gcd.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Games\Gcd;
+
+use function cli\line;
+
+function run($name)
+{
+    $number1 = rand(1, 100);
+    $number2 = rand(1, 100);
+
+    $a = $number1;
+    $b = $number2;
+    while ($a != 0 && $b != 0) {
+        if ($a > $b) {
+            $a = $a % $b;
+        } else {
+            $b = $b % $a;
+        }
+    }
+    $nod = $a + $b;
+
+    line("Question: %s %s", $number1, $number2);
+    $answer = \cli\prompt('Your answer');
+
+    if ($answer == $nod) {
+        line("Correct!");
+        return true;
+    } else {
+        line("'%s' is wrong answer ;(. Correct answer was '%s'", $answer, $nod);
+        line("Let's try again, %s", $name);
+        return false;
+    }
+}
---
+++ src/games/prime.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Games\Prime;
+
+use function cli\line;
+
+function run($name)
+{
+    $number = rand(1, 1000);
+    $d = 2;
+    while ($number % $d != 0) {
+        $d += 1;
+    }
+    $prime = ($d == $number);
+
+    line("Question: %s", $number);
+    $answer = \cli\prompt('Your answer');
+
+    if (($answer == "yes" && $prime == true) || ($answer == "no" && $prime == false)) {
+        line("Correct!");
+        return true;
+    } else {
+        $correctAnswer = ($prime == true) ? 'yes' : 'no';
+        line("'%s' is wrong answer ;(. Correct answer was '%s'", $answer, $correctAnswer);
+        line("Let's try again, %s", $name);
+        return false;
+    }
+}
